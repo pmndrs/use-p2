92d126772a5b24ad2d1598126d9e70e61446f257
Jörg Jäckel <9137061+joergjaeckel@users.noreply.github.com>
2022-02-22 21:42:49 +0100

    catching up with pmndrs/use-cannon#disallow-logical-assignment-and-nullish-coalescing-operators-220119

--- src/worker/material.ts
+++ src/worker/material.ts
@@ -1,13 +1,17 @@
 import { type MaterialOptions, Material } from 'p2-es'
 
-export type CreateMaterial = (materialOptions?: MaterialOptions | number) => Material
+export type CreateMaterial = (nameOrOptions?: MaterialOptions | number) => Material
 
 let materialId = 0
 
 export const createMaterialFactory =
   (materials: Record<number, Material>): CreateMaterial =>
-  (materialOptions = {}) => {
-    const { id = materialId++ }: MaterialOptions =
-      typeof materialOptions === 'number' ? { id: materialOptions } : { ...materialOptions }
-    return (materials[id] ||= new Material(id))
+  (nameOrOptions = {}) => {
+      const materialOptions =
+          typeof nameOrOptions === 'number'
+              ? { id: nameOrOptions }
+              : { ...nameOrOptions } //name: Symbol.for(`Material${materialId++}`),
+      const { id = materialId++ } = materialOptions
+      materials[id] = materials[id] || new Material(id)
+      return materials[id]
   }
